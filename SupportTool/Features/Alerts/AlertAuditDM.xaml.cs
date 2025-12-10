2435c5c909f2bcc4e1d43a70a73f8ac457873dae
bSienkiewicz-auc <bartosz.sienkiewicz@auctane.com>
2025-12-10 12:54:54 +0100

    feat: add base AlertAuditDM page

---
+++ SupportTool/Features/Alerts/AlertAuditDM.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace SupportTool.Features.Alerts;
+
+public sealed partial class AlertAuditDM : Page
+{
+    public AlertAuditDM()
+    {
+        InitializeComponent();
+    }
+}
